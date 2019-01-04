27d09e24cab2a30db8f6c0541ea3833ebc1a75d8
Liam Nestelroad <liam@Liams-MacBook-Pro-6.local>
2019-01-04 01:30:18 -0700

    changes made for raspi filesystem

--- quickstart.py
+++ quickstart.py
@@ -1,41 +1,3 @@
-# from __future__ import print_function
-# from googleapiclient.discovery import build
-# from httplib2 import Http
-# from oauth2client import file, client, tools
-
-# # If modifying these scopes, delete the file token.json.
-# SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
-
-# # The ID and range of a sample spreadsheet.
-# SAMPLE_SPREADSHEET_ID = '1wd-SJIKFnhZ15XAFvIoh5tVSlYQGeoRpTpTQQKLoiGQ'
-# SAMPLE_RANGE_NAME = 'Class Data!A2:E'
-
-# def main():
-#     """Shows basic usage of the Sheets API.
-#     Prints values from a sample spreadsheet.
-#     """
-#     # The file token.json stores the user's access and refresh tokens, and is
-#     # created automatically when the authorization flow completes for the first
-#     # time.
-#     store = file.Storage('token.json')
-#     creds = store.get()
-#     if not creds or creds.invalid:
-#         flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
-#         creds = tools.run_flow(flow, store)
-#     service = build('sheets', 'v4', http=creds.authorize(Http()))
-
-#     values = [
-#         [
-#             Sheet1!A1 #         ]
-#     ]
-#     body = {
-#         'values': values
-#     }
-#     result = service.spreadsheets().values().update(
-#         spreadsheetId=SAMPLE_SPREADSHEET_ID, range=range_name,
-#         valueInputOption=value_input_option, body=body).execute()
-#     print('{0} cells updated.'.format(result.get('updatedCells')))
-
 from __future__ import print_function
 import sqlite3
 import time
@@ -45,13 +7,11 @@ from apiclient import discovery
 from httplib2 import Http
 from oauth2client import file, client, tools
 
-pwd = sys.argv[2] + '/'
-
 SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
-store = file.Storage('/Users/liam/Documents/Code_Projects/Raspberry_Pi/AutoIP/storage.json')
+store = file.Storage('/home/pi/RaspiHeadless/storage.json')
 creds = store.get()
 if not creds or creds.invalid:
-    flow = client.flow_from_clientsecrets('/Users/liam/Documents/Code_Projects/Raspberry_Pi/AutoIP/credentials.json', SCOPES)
+    flow = client.flow_from_clientsecrets('/home/pi/RaspiHeadless/credentials.json', SCOPES)
     creds = tools.run_flow(flow, store)
 SHEETS = discovery.build('sheets', 'v4', http=creds.authorize(Http()))
 
@@ -65,7 +25,12 @@ ip = (str(sys.argv[1]))
 values = [
         ["Hostname", "IP Adress", "Last Update"],
         [],
-        ["Liam's MBP IP:", ip, time.ctime()],
+        [],
+        [],
+        [],
+        [],
+        [],
+        ["TBD", ip, time.ctime()]
     ]
 body = {'values': values }
 
--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 echo "Working"
-dir=$(pwd)
 ip=$(/sbin/ifconfig | grep "inet " | grep -v 127.0.0.1 | awk '{print $2}')
-/usr/local/bin/python3 /Users/liam/Documents/Code_Projects/Raspberry_Pi/AutoIP/quickstart.py $ip $dir > /tmp/pyLogs.txt
+python3 /home/pi/RaspiHeadless/quickstart.py $ip > /tmp/pyLogs.txt
