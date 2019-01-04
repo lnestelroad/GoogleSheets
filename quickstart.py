84d3dbd3f582babe12049f3ebf5ee0c2a4c3c651
Liam Nestelroad <liam@Liams-MacBook-Pro-6.local>
2019-01-04 12:38:42 -0700

    Modified files for raspi file system

--- quickstart.py
+++ quickstart.py
@@ -1,3 +1,41 @@
+# from __future__ import print_function
+# from googleapiclient.discovery import build
+# from httplib2 import Http
+# from oauth2client import file, client, tools
+
+# # If modifying these scopes, delete the file token.json.
+# SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+
+# # The ID and range of a sample spreadsheet.
+# SAMPLE_SPREADSHEET_ID = '1wd-SJIKFnhZ15XAFvIoh5tVSlYQGeoRpTpTQQKLoiGQ'
+# SAMPLE_RANGE_NAME = 'Class Data!A2:E'
+
+# def main():
+#     """Shows basic usage of the Sheets API.
+#     Prints values from a sample spreadsheet.
+#     """
+#     # The file token.json stores the user's access and refresh tokens, and is
+#     # created automatically when the authorization flow completes for the first
+#     # time.
+#     store = file.Storage('token.json')
+#     creds = store.get()
+#     if not creds or creds.invalid:
+#         flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+#         creds = tools.run_flow(flow, store)
+#     service = build('sheets', 'v4', http=creds.authorize(Http()))
+
+#     values = [
+#         [
+#             Sheet1!A1 #         ]
+#     ]
+#     body = {
+#         'values': values
+#     }
+#     result = service.spreadsheets().values().update(
+#         spreadsheetId=SAMPLE_SPREADSHEET_ID, range=range_name,
+#         valueInputOption=value_input_option, body=body).execute()
+#     print('{0} cells updated.'.format(result.get('updatedCells')))
+
 from __future__ import print_function
 import sqlite3
 import time
@@ -26,11 +64,7 @@ values = [
         ["Hostname", "IP Adress", "Last Update"],
         [],
         [],
-        [],
-        [],
-        [],
-        [],
-        ["TBD", ip, time.ctime()]
+        ["Raspi3's IP:", ip, time.ctime()]
     ]
 body = {'values': values }
 
--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 echo "Working"
 ip=$(/sbin/ifconfig | grep "inet " | grep -v 127.0.0.1 | awk '{print $2}')
-python3 /home/pi/RaspiHeadless/quickstart.py $ip > /tmp/pyLogs.txt
+python3 /home/pi/RaspiHeadless/quickstart.py $ip
